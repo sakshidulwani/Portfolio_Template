9f75b8ae9b96a4c8da9e9cbfe68121518cd1802d
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-14 23:21:15 +0530

    changes

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ $('.count').counterUp({
     delay: 10,
     time: 3000
 })
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+//when the user scrolls down 20px from the top of the document, show the button 
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+
+    } else {
+        mybutton.style.display = "none"
+    }
+}
+//When the user click on the button , scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
